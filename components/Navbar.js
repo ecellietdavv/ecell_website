e030c75f8927483d4bb43781d9a1243bdc94f942
Mohit Aasirwal <mohitaasirwal@gmail.com>
2022-09-08 22:35:47 +0530

    half navbar addons should be made

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import logo from '../assets/images/images-event/Ecell Logo (B).png'
 
 function Navbar() {
     const handleDarkMode = () => {
@@ -9,12 +12,30 @@ function Navbar() {
             document.documentElement.classList.remove('dark')
         }
     }
+    const mode=()=>{
+        localStorage.theme=='dark'?mode="dark":mode='light'
+        return mode;
+    }
 
     return (
-        <div className='flex justify-between px-10 w-full'>
-            <h1>Ecell</h1>
-            <button type='button' onClick={handleDarkMode}>Dark Mode</button>
-        </div>
+        <>
+        <div className='flex justify-between px-5 w-full'>
+        <nav className="bg-gray-50 border-b-mid rounded border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+    
+  <div className="container flex flex-start">
+  <ul>
+    <li className=' px-4'><a href="./" className='text-dark hover:underline hover:text-mid dark:text-light dark:hover:text-brand after:text-light'><i className="lni lni-home"></i> Home </a></li>
+    <li className=' px-4'><a href="./" className='text-dark hover:underline hover:text-mid dark:text-light dark:hover:text-brand after:text-light'><i className="lni lni-agenda"></i> About </a></li>
+    <li className=' px-4'><a href="./" className='text-dark hover:underline hover:text-mid dark:text-light dark:hover:text-brand after:text-light'> <i className="las la-user-friends"></i> Our Team </a></li>
+    <li className=' px-4'><a href="./" className='text-dark hover:underline hover:text-mid dark:text-light dark:hover:text-brand after:text-light'><i className="lar la-calendar"></i> Events </a></li>
+<li> <button type='button' onClick={handleDarkMode} className="waves-effect waves-dark btn-flat dark:text-light mr-20">{mode} Mode</button></li>
+  </ul>
+  {/* <Image src={logo} width={100} height={100} alt="Logo"/> */}
+ 
+  </div>
+  </nav>
+</div>
+</>
     )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
 import Head from "next/head"
+import Link from "next/link"
+import Script from "next/script"
 
 function HomePage() {
   return (
     <div className="bg-light dark:bg-dark w-full h-screen">
       <Head>
-        <meta http-equiv="X-UA-Compatible" content="IE=7" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=7" />
         <meta name="description" content="" />
-      </Head>
+        <meta name="keywords" content="ecell, E-cell, ietdavv, ecelliet, ietclubs, ietsocieties, ecelldavv, davv, ietdavv, engineering davv, ietdavv.edu.in, clubs in ietdavv, engineering, science, enterpreneurship, tech, web, html, jsx, css, nextjs, colleges in indore, colleges with ecell"/>
+        <link rel="shortcut icon" href="assets\images\images-event\Ecell Logo (W).png" type="image/x-icon" />
+      </Head>     
     </div>
   )
 }
