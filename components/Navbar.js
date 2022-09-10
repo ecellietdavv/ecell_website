3f04d2f834bb3859f762a70474a80a7480f6d5cb
Mohit Aasirwal <mohitaasirwal@gmail.com>
2022-09-10 22:55:49 +0530

    navbar changes

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,6 +5,7 @@ import logo from "../assets/images/images-event/Ecell Logo (B).png";
 import WbSunnyIcon from "@mui/icons-material/WbSunny";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
 import MenuIcon from '@mui/icons-material/Menu';
+import {AiOutlineInfoCircle,AiOutlineForm,AiOutlinePhone, AiFillHome,AiOutlineTeam } from "react-icons/ai";
 
 function Navbar() {
   const [dark, setDark] = useState("false");
@@ -24,18 +25,20 @@ function Navbar() {
   };
 
   const navItems = [
+    { name: "Home",icon: <AiFillHome/>, link: "" },
     { name: "About", link: "" },
-    { name: "Contact", link: "" },
-    { name: "YT", link: "" },
-    { name: "Good", link: "" },
+    { name: "Our Team", link: "" },
+    { name: "Events", link: "" },
   ];
 
   return (
     <nav className="bg-light dark:bg-dark w-full py-4 flex dark:text-white text-dark justify-between px-4 fixed top-0">
-      <p>Ecell</p>
+      <div className="block">
+      logo svg
+    </div>
       <ul className="space-x-4 md:flex hidden px-4">
         {navItems.map((value, idx) => {
-          return <li key={idx}>{value.name}</li>;
+          return <li key={idx}>{value.icon}{value.name}</li>;
         })}
       </ul>
       <div className="flex space-x-4 items-center justify-center px-4">
@@ -44,7 +47,7 @@ function Navbar() {
         ) : (
           <WbSunnyIcon className="cursor-pointer" onClick={handleDarkMode} />
         )}
-        <button className="md:block hidden bg-brand py-2 px-4 rounded-full text-white">Contact Us</button>
+        <button className="md:block hidden bg-brand-400 dark:bg-brand-400 hover:bg-brand-600 py-2 px-4 rounded-full text-dark dark:text-white">Contact Us</button>
         <MenuIcon
           className="block md:hidden"
           data-drawer-target="drawer-navigation" data-drawer-show="drawer-navigation" aria-controls="drawer-navigation"
