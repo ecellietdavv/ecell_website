4b11efddad388d7419a7ea2666997b10cc942112
Shivang Mishra <63797338+ShivangM@users.noreply.github.com>
2022-09-11 23:11:01 +0530

    Events Fixed

--- components/Event.js
+++ components/Event.js
@@ -1,32 +1,41 @@
 import React from "react";
-import PlayForWorkIcon from "@mui/icons-material/PlayForWork";
-import UpgradeIcon from "@mui/icons-material/Upgrade";
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 import { useState } from "react";
+import Image from "next/image";
+
 const Event = () => {
-	const [show, setShow] = useState("false");
 	const card = [
-		{ img: "NEC2020.eee471b5.jpeg", cont: "lorem20jjkshvdhfuhihhn" },
-		{ img: "NEC2020.eee471b5.jpeg", cont: "ldfbdfvb20jjkshvdhfuhihhn" },
-		{ img: "NEC2020.eee471b5.jpeg", cont: "lorem20jcvzvzcvfdjkshvdhfuhihhn" },
+		{ img: "https://xsgames.co/randomusers/avatar.php?g=male", desc: "lorem20jjkshvdhfuhihhn", name: "Event Name" },
+		{ img: "https://xsgames.co/randomusers/avatar.php?g=female", desc: "ldfbdfvb20jjkshvdhfuhi", name: "Event Name" },
+		{ img: "https://xsgames.co/randomusers/avatar.php?g=male", desc: "lorem20jcvzvzcvfdjkshv", name: "Event Name" },
 	];
-	//         let elem = document.getElementById("box");
-	//         function slide() {
-	//         elem.classList.toggle('hide');
-	// }
-	// let hidee = document.getElementById("hide");
-	// const description = () => {
-	// 	if (show == "false") {
-	// 		hidee.classList.add("h-0");
-	// 		setShow("true");
-	// 	} else {
-	// 		hidee.classList.remove("h-12");
-	// 		setShow("false");
-	// 	}
-	// };
+
+	const EventCard = ({ img, name, desc, idx }) => {
+		const [show, setShow] = useState(false)
+		return (
+			<div className="flex flex-col text-center text-dark dark:text-white justify-start h-full space-y-2" key={idx}>
+
+				<div className="h-52 bg-white overflow-hidden">
+					<Image src={img} alt={name} height={400} width={600} className="object-cover"/>
+				</div>
+
+				<h3>{name}</h3>
+				{
+					show ?
+						<ArrowDropUpIcon className="text-4xl text-center w-full cursor-pointer" onClick={() => setShow(!show)} />
+						:
+						<ArrowDropDownIcon className="text-4xl text-center w-full cursor-pointer" onClick={() => setShow(!show)} />
+				}
+				{show ? <p>{desc}</p> : null}
+			</div>
+		)
+	}
+
 	return (
-		<>
-			<div className="pt-20">
-				<div className="bg-brand-500 space-y-5 pb-5">
+		<section>
+			<div className="mt-10">
+				<div className="bg-brand-500 h-[50vh] flex flex-col items-center justify-center text-white space-y-5">
 					<h3 className="text-4xl text-center font-bold">Events</h3>
 					<p className="text-base w-4/6 text-center mx-auto">
 						Lorem ipsum, dolor sit amet consectetur adipisicing elit. At
@@ -35,33 +44,17 @@ const Event = () => {
 						voluptate deleniti eligendi odit fugit nemo tempore atque nisi ab!
 					</p>
 				</div>
-				<div className="grid grid-cols-3 space-x-3 mx-auto">
-					{card?.map((value, ind) => {
+
+				<div className="grid justify-center px-10 md:px-20 items-center dark:bg-dark grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mx-auto py-10">
+					{card?.map((value, idx) => {
+						const { img, name, desc } = value
 						return (
-							<div className="mx-auto align-middle justify-center items-center" key={ind}>
-								<img src="NEC2022.jpeg" className="" alt="" />
-								{show ? (
-									<PlayForWorkIcon
-										className="cursor-pointer mx-auto"
-										// onClick={description}
-									/>
-								) : (
-									<UpgradeIcon
-										className="cursor-pointer mx-auto"
-										// onClick={description}
-									/>
-								)}
-								<div className="hide" id="hide">
-									{value.cont}
-								</div>
-								{/* <button className="" onclick="slide()">hi</button>
-                            <div className="" id="hide">{value.cont}</div> */}
-							</div>
-						);
+							<EventCard img={img} name={name} desc={desc} idx={idx} />
+						)
 					})}
 				</div>
 			</div>
-		</>
+		</section>
 	);
 };
 
--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images:{domains: ['xsgames.co']}
 }
 
 module.exports = nextConfig
