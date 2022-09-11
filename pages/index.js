a11ee6c65499d1b3ee2ae7246261b41263c22047
Shivang Mishra <63797338+ShivangM@users.noreply.github.com>
2022-09-11 20:07:58 +0530

    Unused

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import Head from "next/head"
-import Link from "next/link"
-import Script from "next/script"
 import Drawer from "../components/Drawer"
 import TFCtemplate from "../components/TFCtemplate"
 
