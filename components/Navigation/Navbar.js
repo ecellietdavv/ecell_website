0a74567259fa07d76d4b55fe0fe5fc4b8f001a0c
ShivangM <shivangmishra0824@gmail.com>
2022-10-01 17:40:46 +0530

    Hero Section

--- components/Navigation/Navbar.js
+++ components/Navigation/Navbar.js
@@ -16,6 +16,11 @@ function Navbar() {
   const { dark, handleDarkMode } = appContext
   const { handleToggel } = appContext
 
+  const handleContact = ()=>{
+    handleScroll("footer")
+    handleToggel()
+  }
+
   return (
     <nav className="bg-light dark:bg-dark shadow-lg shadow-mid/20  w-full py-2 flex dark:text-white text-dark fixed z-50 top-0">
       <div className="max-w-7xl flex w-full items-center justify-between mx-auto px-4 sm:px-6">
@@ -38,7 +43,7 @@ function Navbar() {
             <BsSunFill className="cursor-pointer text-2xl" onClick={handleDarkMode} />
           )}
 
-          <button type="button" onClick={() => handleScroll("footer")} className="lg:block hidden bg-brand-400 py-2 px-4 rounded-full bg-gradient-to-r from-brand-500 to-brand-600 hover:to-brand-600 text-white">Contact Us</button>
+          <button type="button" onClick={handleContact} className="lg:block hidden bg-brand-400 py-2 px-4 rounded-full bg-gradient-to-r from-brand-500 to-brand-600 hover:to-brand-600 text-white">Contact Us</button>
 
           <GiHamburgerMenu
             className="block lg:hidden cursor-pointer text-2xl"
