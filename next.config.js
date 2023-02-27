e365afb9487cb54ac50ad2b63c6f9aff2f2aa4b2
ShivangM <shivangmishra0824@gmail.com>
2023-02-27 22:54:36 +0530

    TS Ignore

--- next.config.js
+++ next.config.js
@@ -10,6 +10,16 @@ const nextConfig = {
       'cdn.sanity.io',
     ],
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
