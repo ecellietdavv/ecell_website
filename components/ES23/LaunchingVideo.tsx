d5308dcbbbddffe5b5f996d9d7ee91b61facad13
Huzaifa-code <huzaifaqureshi037@gmail.com>
2023-03-25 15:35:48 +0530

    Video section

--- components/ES23/LaunchingVideo.tsx
+++ components/ES23/LaunchingVideo.tsx
@@ -3,7 +3,12 @@ import React from 'react';
 type Props = {};
 
 const LaunchingVideo = (props: Props) => {
-  return <div className="h-screen">LaunchingVideo</div>;
+  return (
+    <div className="h-screen flex flex-col space-y-16 justify-center items-center">
+      <h1 className='text-4xl font-semibold  text-white '>Reimagine Events the smart way</h1>
+      <iframe width="600" height="350" src="https://www.youtube.com/embed/bNpx7gpSqbY" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+      </iframe>
+    </div>);
 };
 
 export default LaunchingVideo;
