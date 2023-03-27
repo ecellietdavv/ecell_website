9525c41881c9e490988dca72e8a6e47e22214851
Huzaifa-code <huzaifaqureshi037@gmail.com>
2023-03-27 09:57:41 +0530

    modified sponsor section

--- components/ES23/Sponsors.tsx
+++ components/ES23/Sponsors.tsx
@@ -1,51 +1,46 @@
 import Image from 'next/image'
 import React from 'react'
+import { sponsorData } from '../../data/ES23/sponsorData'
 
 type Props = {}
+
+
+type Sponsor = {
+    name: string;
+    img: string;
+}
+
+type SponsorProps = {
+    sponsor: Sponsor;
+    idx: number;
+}
+
+const SponsorImg = ( {sponsor, idx} : SponsorProps ) => {
+    const {name , img} = sponsor;
+
+    return (
+        <div className="relative w-24 h-24">
+            <Image
+                layout="fill"
+                style={{ objectFit: 'cover' }}
+                src={img}
+                alt={name}
+            />
+        </div>
+    );
+}
+
 
 const Sponsors = (props: Props) => {
   return (
     <div className='py-24 bg-dark flex gap-16 justify-center items-center'>
-        <div className="relative w-24 h-24">
-            <Image
-                layout="fill"
-                style={{ objectFit: 'cover' }}
-                src='/assets/ES/Sponsors/uber.svg'
-                alt='sponsors'
-            />
-        </div>
-        <div className="relative w-24 h-24">
-            <Image
-                layout="fill"
-                style={{ objectFit: 'cover' }}
-                src='/assets/ES/Sponsors/uber.svg'
-                alt='sponsors'
-            />
-        </div>
-        <div className="relative w-24 h-24">
-            <Image
-                layout="fill"
-                style={{ objectFit: 'cover' }}
-                src='/assets/ES/Sponsors/uber.svg'
-                alt='sponsors'
-            />
-        </div>
-        <div className="relative w-24 h-24">
-            <Image
-                layout="fill"
-                style={{ objectFit: 'cover' }}
-                src='/assets/ES/Sponsors/uber.svg'
-                alt='sponsors'
-            />
-        </div>
-        <div className="relative w-24 h-24">
-            <Image
-                layout="fill"
-                style={{ objectFit: 'cover' }}
-                src='/assets/ES/Sponsors/uber.svg'
-                alt='sponsors'
-            />
-        </div>
+        {
+            sponsorData.map((sponsor, idx) => {
+                return (
+                    <SponsorImg key={idx} idx={idx} sponsor={sponsor} />
+                )
+            })
+        }
     </div>
   )
 }
