52f5b7641ee59cee87b3eba1b764f06023bb156b
Huzaifa-code <huzaifaqureshi037@gmail.com>
2023-04-01 17:27:00 +0530

    added Sponsors logo

--- data/ES23/sponsorData.ts
+++ data/ES23/sponsorData.ts
@@ -1,18 +1,22 @@
 export const sponsorData = [
    {    
         name : 'uber',
-        img: '/assets/ES/Sponsors/uber.svg',
+        img: '/assets/ES/Sponsors/Chevrolet.webp',
    }, 
    {    
         name : 'uber',
-        img: '/assets/ES/Sponsors/uber.svg',
+        img: '/assets/ES/Sponsors/ktm.webp',
    }, 
    {    
         name : 'uber',
-        img: '/assets/ES/Sponsors/uber.svg',
+        img: '/assets/ES/Sponsors/Swiggy.webp',
    }, 
    {    
         name : 'uber',
-        img: '/assets/ES/Sponsors/uber.svg',
+        img: '/assets/ES/Sponsors/VI.webp',
    }, 
+   {    
+        name : 'uber',
+        img: '/assets/ES/Sponsors/yono.webp',
+   }
 ];
