60bf37125a125182a6dd83b400494f33a9c0730c
Huzaifa-code <huzaifaqureshi037@gmail.com>
2023-04-08 18:43:44 +0530

    updated timeline and speakers

--- data/ES23/speakerData.ts
+++ data/ES23/speakerData.ts
@@ -3,15 +3,6 @@ import { randomPersonImage } from '../../utils/randomAssets';
 export const speakerData = [
   {
     speaker: {
-      name: 'Praful Billore',
-      designation: 'Founder, MBA Chai Wala',
-      img: '/assets/ES/Speakers/praful.png',
-    },
-    eventName: 'Keynote Speaker',
-    eventDate: '04-11-2023',
-  },
-  {
-    speaker: {
       name: 'Anubhav Dubey',
       designation: 'Founder, Chai Sutta Bar',
       img: '/assets/ES/Speakers/anubhav.png',
@@ -39,9 +30,18 @@ export const speakerData = [
   },
   {
     speaker: {
-      name: 'Ishaan Sharma',
-      designation: 'Founder, MarkitUp',
-      img: '/assets/ES/Speakers/ishaan.png',
+      name: 'Sawan S Laddha',
+      designation: 'Founder - Workie growth expert , Forbes 40U40',
+      img: '/assets/ES/Speakers/Sawan\ S\ Laddha.png',
+    },
+    eventName: 'Keynote Speaker',
+    eventDate: '04-12-2023',
+  },
+  {
+    speaker: {
+      name: 'Himanshu Chaturvedi',
+      designation: 'Founder Motiongility',
+      img: '/assets/ES/Speakers/Himanshu Chaturvedi.png',
     },
     eventName: 'Keynote Speaker',
     eventDate: '04-12-2023',
