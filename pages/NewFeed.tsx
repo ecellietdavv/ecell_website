c0f54f3ae404d2bb6946973315b00587ea99dca1
Huzaifa-code <huzaifaqureshi037@gmail.com>
2024-04-11 10:33:49 +0530

    content updated

---
+++ pages/NewFeed.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function NewFeed() {
+  return (
+    <>
+        <h1 className='text-3xl font-semibold'>NewFeed</h1>
+
+        <section>
+            
+        </section>
+    </>
+  )
+}
+
+export default NewFeed
